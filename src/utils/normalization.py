781329db50ade51cba65635f9b9c47278ec28135
Muhammed AbuOdeh <mra9407@nyu.edu>
2025-07-21 18:04:17 +0400

    fixed normalization bug

--- src/utils/normalization.py
+++ src/utils/normalization.py
@@ -6,5 +6,5 @@ from camel_tools.utils.normalize import normalize_teh_marbuta_ar as teh
 def normalize_alef_yeh_ta_line(line):
     return alef(yeh(teh(line)))
 
-def normalize_alef_yeh_ta(lines):
-    return [normalize_alef_yeh_ta_line(line) for line in lines]
+def normalize_alef_yeh_ta(df):
+    df.FORM = df.FORM.apply(normalize_alef_yeh_ta_line)
